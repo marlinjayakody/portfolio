f8a5bbcc0ca311071c614f24bf39585ce892358a
marlinjayakody <marlin.jayakody@gmail.com>
2021-07-05 10:46:14 +0530

    header component

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,30 @@ module.exports = {
       center: true,
       padding: "2rem",
     },
-    extend: {},
+    extend: {
+      colors: {
+        warmgray: {
+           50:'#FAFAF9',
+          100:'#F5F5F5',
+          200: '#E5E5E5',
+          300:'#D4D4D4',
+          400:'#A3A3A3',
+          500:'#737373',
+          600:'#525252',
+          700:'#404040',
+          800: '#262626',
+          900: '#1C1917'
+        }
+        },
+    },
+    fontFamily:{
+      sans:[ 'Red Hat Display', 'sans-serif'],
+      serif:[ 'DM Serif Display', 'serif']
+    },
+    letterSpacing: {
+      wide: '0.025em',
+    }
+    
   },
   variants: {
     extend: {},
