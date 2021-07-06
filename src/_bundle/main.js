b017ab3eb01ea03761af3ec8319ef487b525baaa
marlinjayakody <marlin.jayakody@gmail.com>
2021-07-07 03:12:39 +0530

    scalingjs

--- src/_bundle/main.js
+++ src/_bundle/main.js
@@ -1,24 +1,33 @@
 import "./main.pcss";
 
-window.onload = function() { 
-            var all_links = document.getElementById("nav2").getElementsByTagName("a"),
-                i=0, len=all_links.length,
-                full_path = location.href.split('#')[0]; //Ignore hashes?
-          
-            // Loop through each link.
-            for(; i<len; i++) {
-                if(all_links[i].href.split("#")[0] == full_path) {
-                    all_links[i].className += " border-b-2 border-warmgray-900";
-                }
-            }
-            var all_links = document.getElementById("nav").getElementsByTagName("a"),
-            i=0, len=all_links.length,
-            full_path = location.href.split('#')[0]; //Ignore hashes?
-      
-        // Loop through each link.
-        for(; i<len; i++) {
-            if(all_links[i].href.split("#")[0] == full_path) {
-                all_links[i].className += " font-bold transition-colors duration-200 pr-3 py-2 bg-warmgray-200";
-            }
+window.onload = function () {
+    var all_links = document.getElementById("nav2").getElementsByTagName("a"),
+        i = 0,
+        len = all_links.length,
+        full_path = location.href.split('#')[0]; //Ignore hashes?
+
+    // Loop through each link.
+    for (; i < len; i++) {
+        if (all_links[i].href.split("#")[0] == full_path) {
+            all_links[i].className += " border-b-2 border-warmgray-900";
         }
-          }
+    }
+    var all_links = document.getElementById("nav").getElementsByTagName("a"),
+        i = 0,
+        len = all_links.length,
+        full_path = location.href.split('#')[0]; //Ignore hashes?
+
+    // Loop through each link.
+    for (; i < len; i++) {
+        if (all_links[i].href.split("#")[0] == full_path) {
+            all_links[i].className += " font-bold transition-colors duration-200 pr-3 py-2 bg-warmgray-200";
+        }
+    }
+}
+document.addEventListener('touchmove', function (event) {
+    if (event.scale !== 1) {
+        event.preventDefault();
+    }
+}, {
+    passive: false
+});
